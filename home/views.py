ecc63def294444d8e22ffb7844e824a4caa930d8
Akshay2002Singh <06akshay2002@gmail.com>
2021-11-14 20:44:21 +0530

    push

---
+++ home/views.py
@@ -0,0 +1,32 @@
+from django.http import response
+from django.shortcuts import render
+from home.models import contact
+from django.contrib import messages
+import os
+from django.http.response import HttpResponse
+import mimetypes
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def e_books(request):
+    return render(request,"e_books.html")
+
+def buy_books(request):
+    return render(request,"buy_books.html")
+
+def contact_us(request):
+    return render(request,"contact_us.html")
+
+def submit_contact_form(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        temp_contact = contact(name=name,email=email,phone=phone,message=message)
+        temp_contact.save()
+        messages.success(request,"Your form has been submitted succesfully")
+        return render(request,'contact_us.html')
+

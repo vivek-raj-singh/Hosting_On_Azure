cddab1369f0578c831c8380d424fcb739bd68c97
Akshay2002Singh <06akshay2002@gmail.com>
2021-11-16 02:18:12 +0530

    pus

--- elibrary/urls.py
+++ elibrary/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('home.urls')),
+    path('', include('home.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]

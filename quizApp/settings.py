67136352da58e344fb6a872ece7cf5d8debb2978
Akshay2002Singh <06akshay2002@gmail.com>
2022-11-19 19:30:39 +0530

    add csrf origin

--- quizApp/settings.py
+++ quizApp/settings.py
@@ -132,3 +132,9 @@ MEDIA_URL="media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# csrf manually added
+CSRF_TRUSTED_ORIGINS = [
+    'https://acm-quiz-app.azurewebsites.net'
+]
